0e09193cea73afe244aaa5d4a3a091e71ea4b348
Gergana Hristanova <gergana.hristanova@gmail.com>
2025-10-23 01:14:36 +0300

    Comment out solutions for exercises 6-9
    The code for exercises 6 through 9 in main.cpp has been commented out, possibly for review or debugging purposes. No functional code changes were made; only code deactivation.

--- practicums/year1/pract03/main.cpp
+++ practicums/year1/pract03/main.cpp
@@ -156,24 +156,106 @@ int main()
 
     // --- Ex 6 ---
 
-    int n;
-    std::cin >> n;
+    // int n;
+    // std::cin >> n;
 
-    int digitsCount = 0;
-    int nCopy = n;
-    while (nCopy != 0)
-    {
-        digitsCount++;
-        nCopy /= 10;
-    }
+    // int digitsCount = 0;
+    // int nCopy = n;
+    // while (nCopy != 0)
+    // {
+    //     digitsCount++;
+    //     nCopy /= 10;
+    // }
 
-    int mid = digitsCount / 2;
-    int result = 0;
-    for (int i = 1; i < digitsCount; i++)
-    {
-        result += 10 * i * (n % 10);
-        n /= 10;
-    }
+    // if (digitsCount == 1)
+    // {
+    //     std::cout << 0;
+    // }
+    // else
+    // {
+    //     int mid1 = digitsCount / 2;
+    //     int mid2 = digitsCount / 2;
+    //     if (digitsCount % 2 == 0)
+    //     {
+    //         mid2++;
+    //     }
 
-    std::cout << result;
+    //     //using divisor in a loop since pow() hasn't been learned yet
+    //     int divisor = 1;
+    //     for (int i = 1; i < digitsCount; i++)
+    //     {
+    //         divisor *= 10;
+    //     }
+
+    //     for (int i = 0; i < digitsCount; i++)
+    //     {
+    //         int digit = n / divisor;
+            
+    //         if (i != mid1 - 1 && i != mid2 - 1)
+    //         {
+    //             std::cout << digit;
+    //         }
+
+    //         n %= divisor;
+    //         divisor /= 10;
+    //     }
+    // }
+
+    // --- Ex 7 ---
+
+    // int n;
+    // std::cin >> n;
+
+    // bool isPowerOf2 = false;
+    // for (int i = 2; i <= n; i *= 2)
+    // {
+    //     if (i == n)
+    //     {
+    //         isPowerOf2 = true;
+    //         break;
+    //     }
+    // }
+
+    // std::cout << isPowerOf2;
+
+    // --- Ex 8 ---
+    //a * x^4 - b * x^3 + c x^2 - d * x + e?
+
+    // int a, b, c, d, e;
+    // std::cin >> a >> b >> c >> d >> e;
+
+    // for (int x = -100; x <= 100; x++)
+    // {
+    //     //not sure if that's the polinome
+    //     int polinome = a * x * x * x * x
+    //                  - b * x * x * x
+    //                  + c * x * x
+    //                  - d * x
+    //                  + e;
+        
+    //     if (polinome == 0)
+    //     {
+    //         std::cout << x << " ";
+    //     }
+    // }
+
+    // --- Ex 9 ---
+
+    // One of the in-out examples seems to be wrong.
+
+    // int n;
+    // std::cin >> n;
+
+    // int max = -1;
+    // int scndMax = -1;
+    // for (int i = 0; i < n; i ++)
+    // {
+    //     int currentNum;
+    //     std::cin >> currentNum;
+
+    //     max = (currentNum > max) ? currentNum : max;
+    //     scndMax = (currentNum > scndMax && currentNum < max) ? currentNum : scndMax;
+    // }
+
+    // std::cout << max << " " << scndMax;
 }
