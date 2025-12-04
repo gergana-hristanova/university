d787945bacd33147e607de1fefa1ecfd2675cc52
Gergana Hristanova <gergana.hristanova@gmail.com>
2025-12-04 10:34:46 +0200

    Refactor code structure for improved readability and maintainability

--- student/semester1/lectures/lect10/quickSort.cpp
+++ student/semester1/lectures/lect10/quickSort.cpp
@@ -3,9 +3,20 @@
 // Split the elements in arr so those smaller than pivot go left, the others go right.
 void split(int arr[], size_t size, int pivot)
 {
-    for (int i = 0; i < pivot; i++)
+    unsigned current = 0; // all right of current are < pivot
+    unsigned search = current + 1; //next pivot candidate
+
+    while (current < size)
     {
-        
+        if (current <= pivot)
+        {
+            ++current;
+        }
+        else
+        {
+            if (arr[search] <= pivot)
+                std::swap(arr[current], arr[search]);
+        }
     }
 }
 
@@ -16,7 +27,18 @@ void quickSort(int arr[], size_t size)
 
 }
 
+void printArr(const int arr[], size_t size)
+{
+    for (size_t i = 0; i < size; i++)
+        std::cout << arr[i] << " ";
+    std::cout << std::endl;
+}
+
 int main()
 {
+    int arr[] = { 2, 3, 6, 8, 1, 9 };
 
+
+
+    printArr(arr, 6);
 }
