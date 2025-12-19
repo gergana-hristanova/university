e51f3397c5aa0b473b3d21499a8a3f2554542fb8
Gergana Hristanova <gergana.hristanova@gmail.com>
2025-12-19 21:34:22 +0200

    Finished Task 2 in hom01

--- student/semester1/homework/hom01/Task_2_9MI0800780.cpp
+++ student/semester1/homework/hom01/Task_2_9MI0800780.cpp
@@ -6,15 +6,15 @@ using namespace std;
 constexpr size_t MAX_SIZE = 20;
 unsigned board[MAX_SIZE][MAX_SIZE] = { 0 };
 
-size_t N = 4; //board size
-unsigned K = 11; //win condition
+size_t N = 4;
+unsigned K = 11;
 
 bool hasQuit = false;
 bool isPlaying = false;
 
 void clearConsole()
 {
-    system("clear"); //change to "cls" if on Windows / "clear" if on Linux/MaxOS
+    system("clear"); //cls for Windows
 }
 
 void printBoard()
@@ -27,7 +27,6 @@ void printBoard()
         {
             cout << "|  " << board[i][j] << "  ";
         }
-
         cout << "|" << endl;
     }
 }
@@ -37,8 +36,69 @@ int randFromTo(unsigned from, unsigned to)
     return from + rand() % (to - from + 1);
 }
 
+int strcmp(const char* a, const char* b)
+{
+    if (!a || !b) return 0;
+
+    while (*a && (*a == *b))
+    {
+        a++;
+        b++;
+    }
+    return *a - *b;
+}
+
+bool hasEmptyCell()
+{
+    for (size_t i = 0; i < N; i++)
+        for (size_t j = 0; j < N; j++)
+            if (board[i][j] == 0)
+                return true;
+    return false;
+}
+
+bool boardsEqual(unsigned a[][MAX_SIZE], unsigned b[][MAX_SIZE])
+{
+    for (size_t i = 0; i < N; i++)
+        for (size_t j = 0; j < N; j++)
+            if (a[i][j] != b[i][j])
+                return false;
+    return true;
+}
+
+bool checkWin()
+{
+    unsigned target = 1 << K;
+    for (size_t i = 0; i < N; i++)
+        for (size_t j = 0; j < N; j++)
+            if (board[i][j] == target)
+                return true;
+    return false;
+}
+
+bool canMove()
+{
+    if (hasEmptyCell()) return true;
+
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < N; j++)
+        {
+            if (i + 1 < N && board[i][j] == board[i + 1][j])
+                return true;
+
+            if (j + 1 < N && board[i][j] == board[i][j + 1])
+                return true;
+        }
+    }
+
+    return false;
+}
+
 void addNewNum()
 {
+    if (!hasEmptyCell()) return;
+
     size_t i = randFromTo(0, N - 1);
     size_t j = randFromTo(0, N - 1);
 
@@ -47,26 +107,21 @@ void addNewNum()
         addNewNum();
         return;
     }
-    
-    const unsigned probability = randFromTo(1, 10);
-    if (probability == 1)
-        board[i][j] = 4;
-    else
-        board[i][j] = 2;
+
+    board[i][j] = (randFromTo(1, 10) == 1) ? 4 : 2;
 }
 
-void compressRow(unsigned row[])
+void compressRow(unsigned row[]) 
 {
-    unsigned result[N] = { 0 };
-    size_t posInResult = 0;
+    unsigned result[MAX_SIZE] = { 0 };
+    size_t pos = 0;
 
     for (size_t i = 0; i < N; i++)
     {
         unsigned curr = row[i];
         if (curr != 0)
         {
-            result[posInResult] = curr;
-            posInResult++;
+            result[pos++] = curr;
         }
     }
 
@@ -80,7 +135,7 @@ void mergeInRow(unsigned row[])
 {
     for (size_t i = 0; i < N - 1; i++)
     {
-        if (row[i] == row[i + 1])
+        if (row[i] != 0 && row[i] == row[i + 1])
         {
             row[i] *= 2;
             row[i + 1] = 0;
@@ -92,74 +147,75 @@ void mergeInRow(unsigned row[])
 void reverseRow(unsigned row[])
 {
     for (size_t i = 0; i < N / 2; i++)
-    {
         swap(row[i], row[N - i - 1]);
-    }
 }
 
-void transposeBoard() {
-    for (size_t i = 0; i < N; i++) {
-        for (size_t j = i + 1; j < N; j++) {
+void transposeBoard()
+{
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = i + 1; j < N; j++)
+        {
             swap(board[i][j], board[j][i]);
         }
     }
 }
 
-void processRow(unsigned row[]) {
+void processRow(unsigned row[])
+{
     compressRow(row);
     mergeInRow(row);
     compressRow(row);
 }
 
-void moveLeft() {
+void moveLeft()
+{
     for (size_t i = 0; i < N; i++)
         processRow(board[i]);
 }
 
-void moveRight() {
-    for (size_t i = 0; i < N; i++) {
+void moveRight()
+{
+    for (size_t i = 0; i < N; i++)
+    {
         reverseRow(board[i]);
         processRow(board[i]);
         reverseRow(board[i]);
     }
 }
 
-void moveUp() {
+void moveUp()
+{
     transposeBoard();
     moveLeft();
     transposeBoard();
 }
 
-void moveDown() {
+void moveDown()
+{
     transposeBoard();
     moveRight();
     transposeBoard();
 }
 
-int strcmp(const char* a, const char* b)
-{
-    if (!a || !b) return 0;
-
-    while (*a && *a == *b)
-    {
-        a++;
-        b++;
-    }
-
-    return *a - *b;
-}
-
-bool boardsEqual(unsigned a[][MAX_SIZE], unsigned b[][MAX_SIZE], size_t N) {
-    for (size_t i = 0; i < N; i++)
-        for (size_t j = 0; j < N; j++)
-            if (a[i][j] != b[i][j])
-                return false;
-    return true;
-}
-
 void handleGameInput()
 {
-    while (true) {
+    while (true)
+    {
+        if (checkWin())
+        {
+            printBoard();
+            cout << "Game won! Reached " << (1 << K) << "!" << endl;
+            break;
+        }
+
+        if (!canMove())
+        {
+            printBoard();
+            cout << "Game over! No moves left." << endl;
+            break;
+        }
+
         cout << endl << "Enter left/right/up/down:" << endl;
 
         char gameInput[16];
@@ -179,7 +235,7 @@ void handleGameInput()
         else if (strcmp(gameInput, "down") == 0)
             moveDown();
 
-        if (!boardsEqual(board, boardCopy, N))
+        if (!boardsEqual(board, boardCopy))
             addNewNum();
 
         printBoard();
@@ -190,9 +246,9 @@ void startGame()
 {
     clearConsole();
 
-    size_t i = randFromTo(0, N - 1);
-    size_t j = randFromTo(0, N - 1);
-    board[i][j] = 2;
+    size_t i1 = randFromTo(0, N - 1);
+    size_t j1 = randFromTo(0, N - 1);
+    board[i1][j1] = 2;
 
     printBoard();
     handleGameInput();
@@ -201,15 +257,14 @@ void startGame()
 void adjustSettings()
 {
     clearConsole();
-    
     cout << "=== SETTINGS ===" << endl;
 
     cout << "Enter board size (currently " << N << "):" << endl;
     size_t boardSize;
     cin >> boardSize;
-    while (!(3 <= boardSize && boardSize <= 20))
+    while (!(3 <= boardSize && boardSize <= MAX_SIZE))
     {
-        cout << "Invalid board size! Enter again:" << endl;
+        cout << "Board size must be (3... " << MAX_SIZE << "). Enter again:" << endl;
         cin >> boardSize;
     }
 
@@ -218,7 +273,7 @@ void adjustSettings()
     cin >> winCond;
     while (!(4 <= winCond && winCond <= 31))
     {
-        cout << "ERROR! Invalid win condition!" << endl;
+        cout << "Win condition must be (4...31). Enter again:" << endl;
         cin >> winCond;
     }
 
@@ -236,11 +291,9 @@ void handleHomescreenInput()
         case 1:
             isPlaying = true;
             break;
-
         case 2:
             adjustSettings();
             break;
-
         case 3:
             hasQuit = true;
             break;
@@ -250,8 +303,11 @@ void handleHomescreenInput()
 void homescreen()
 {
     clearConsole();
-    
-    cout << "=== WELCOME TO 2048 ===" << endl << "1 - Start Game" << endl << "2 - Settings" << endl << "3 - Quit" << endl;
+
+    cout << "=== WELCOME TO 2048 ===" << endl
+         << "1 - Start Game" << endl
+         << "2 - Settings" << endl
+         << "3 - Quit" << endl;
 
     handleHomescreenInput();
 
@@ -261,7 +317,7 @@ void homescreen()
 
 int main()
 {
-    srand(time(0)); //neccesary for rand() to work
+    srand(time(0)); // needed for rand() to work
 
     homescreen();
 
@@ -269,10 +325,5 @@ int main()
         startGame();
 
     cout << "Quitting game...";
+    return 0;
 }
-
-// Може ли да ползваме функция за четене на цял ред от конзолата за 3 зад? ---да
-// Може ли да ползваме srand(time(0)) във 2 зад?
-// Може ли да ползваме проверка от вида if(cin >> n) за валидация на входни данни? ---няма нужда, защото при въведен текст и непроверено такова няма да е санкция
-// Може ли да имаме вложени цикли при обхождане на матрицата във 2 зад? ---да
-// Грешен ли е примерът в 3 зад? ---да, трябва да е 100 вместо 10
